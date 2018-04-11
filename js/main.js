141bd95dfd3acd4c7a40e13224440cbdfe45674b
weiweizh <weiweizh@kth.se>
2018-04-11 11:33:45 -0700

    Fixing the filters on index page

--- js/main.js
+++ js/main.js
@@ -6,25 +6,8 @@
 (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/fex7h07a';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
 
 
+
 jQuery(document).ready(function($){
-	//open/close lateral filter
-
-//$("#aboutMe").on('click',function(){
-
-  //  $(".overlay").attr('style',"visibility: hidden");
-
-
-
-    //$("#aboutMe").attr('style',"visi")
-
-  //});
-
-  //$("#More").on('click',function(){
-//  $(".overlay").attr('style',"visibility: visible");
-//  });
-
-
-
 
 	$('.index-filter-trigger').on('click', function(){
 		triggerFilter(true);
